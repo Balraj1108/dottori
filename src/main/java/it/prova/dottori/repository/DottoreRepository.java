bd7db39092e479ce4a9c377f87b5d932b955c203
Balraj1108 <balrajsingh1108@gmail.com>
2022-12-01 23:50:42 +0100

    Implementazione DottoreRepository

---
+++ src/main/java/it/prova/dottori/repository/DottoreRepository.java
@@ -0,0 +1,16 @@
+package it.prova.dottori.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import it.prova.dottori.model.Dottore;
+
+public interface DottoreRepository extends CrudRepository<Dottore, Long>{
+	
+	@Query("from Dottore d where d.codFiscalePazienteAttualmenteInVisita = :codiceFiscale")
+	Dottore findByCodFiscale(String codiceFiscale); 
+	
+	@Query("from Dottore d where d.codiceDottore = :codiceDottore")
+	Dottore verifyDisponibilita(String codiceDottore);
+	
+}
